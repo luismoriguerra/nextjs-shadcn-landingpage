dde609d367883094241bde9c5422529aea0dabdd
Luis Mori Guerra <luismorith@gmail.com>
2024-07-11 19:51:45 -0500

    feat : added example
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { Main, Section, Container } from "@/components/craft";
+import Hero from "@/components/home-page/hero";
+import FeatureLeft from "@/components/home-page/feature-left";
+import FeatureRight from "@/components/home-page/feature-right";
 
 export default function Home() {
   return (
     <div>
-      hello
+      <Main>
+        <Section>
+          <Container>
+            <Hero />
+            <FeatureLeft />
+            <FeatureRight />
+          </Container>
+        </Section>
+      </Main>
     </div>
   );
 }
